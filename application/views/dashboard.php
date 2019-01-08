4e979add42caf765121f75b32b7a5b4d6e2df798
salsanaliafthin <35424301+salsanaliafthin@users.noreply.github.com>
2019-01-08 14:56:17 +0700

    masih dashboard admin

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -154,8 +154,8 @@
                                 PANEL ADMIN
                             </li>
                             <li class="nav-item ">
-                                <a class="nav-link active" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-4" aria-controls="submenu-4"><i class="fa fa-fw fa-user-circle"></i>Dashboard <span class="badge badge-success">6</span></a>
-                                <div id="submenu-4" class="collapse submenu" style="">
+                                <a class="nav-link active" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-3" aria-controls="submenu-3"><i class="fa fa-fw fa-user-circle"></i>Dashboard <span class="badge badge-success">6</span></a>
+                                <div id="submenu-3" class="collapse submenu" style="">
                                     <ul class="nav flex-column">
                                         <li class="nav-item">
                                             <a class="nav-link" href="dashboard-sales.html">Detail Admin</a>
@@ -170,6 +170,7 @@
                                 <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-4" aria-controls="submenu-4"><i class="fab fa-fw fa-wpforms"></i>Pegawai</a>
                                 <div id="submenu-4" class="collapse submenu" style="">
                                     <ul class="nav flex-column">
+
                                         <li class="nav-item">
                                             <a class="nav-link" href="pages/form-elements.html">Detail Pegawai</a>
                                         </li>
